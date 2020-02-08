5957c5e36212cce92d49925bbd58e37d5d3f2ff5
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-02-08 13:11:25 +0000

    add main class

---
+++ main.py
@@ -0,0 +1,35 @@
+"""
+Main class
+
+--
+
+Birth date : 08/02/20 
+
+Author : DrLarck
+
+Last update : 08/02/20 (DrLarck)
+"""
+
+class Main():
+    """
+    Represents the main class of the program which runs it.
+
+    - Method :
+
+    `run()` : Connect the bot
+    """
+
+    # method
+    def run(self):
+        """
+        Connect the bot
+
+        --
+
+        Return : `None`
+        """
+
+        return
+
+if(__name__ == "__main__"):
+    Main().run()
