4ddf970dbe94457cf51b16b7d893e5693c31522a
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-02-08 14:00:42 +0000

    add user object

---
+++ utility/user/user.py
@@ -0,0 +1,28 @@
+"""
+User object
+
+--
+
+Author : DrLarck
+
+Last update : 08/02/20 (DrLarck)
+"""
+
+# dependancies
+
+class User():
+    """
+    Represents an user
+
+    - Attribute :
+
+    `name` (`str`) : The user's name
+
+    `id` (`int`) : The user's `discord id`
+    """
+
+    # attribute
+    def __init__(self):
+        # info
+        self.name = ""
+        self.id = 0
